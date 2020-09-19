52e326432043c8f7e66e1ab533e37aeec85a6448
ujjwal <ujjwal.jain@vanderbilt>
2020-09-20 03:56:30 +0530

    frontend

---
+++ src/services/PostData.js
@@ -0,0 +1,18 @@
+export function PostData(type, userData) {
+    let BaseURL = 'http://localhost/react-php/api/index.php';
+    return new Promise((resolve, reject) =>{
+    
+        fetch(BaseURL+'?tp='+type,
+        {
+            method: 'POST',
+            body:JSON.stringify(userData)
+        })
+        .then((response) => response.json()
+        .then((res) => {
+            resolve(res);
+        }))
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
